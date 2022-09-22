cd54f73b9d50d29aa54518caf9298784961f4fbc
Sadaf Mulla <68865067+sadafmulla@users.noreply.github.com>
2022-09-22 14:21:03 +0530

    Add files via upload

---
+++ Assignment4/Ass4q1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <omp.h>
+#include <time.h>
+
+int fib(int n)
+{
+  int i, j;
+  if (n<2)
+    return n;
+  else
+    {
+       #pragma omp task shared(i) firstprivate(n)
+       i=fib(n-1);
+
+       #pragma omp task shared(j) firstprivate(n)
+       j=fib(n-2);
+
+       #pragma omp taskwait
+       return i+j;
+    }
+}
+
+int main()
+{
+	clock_t start, finish;
+    start = clock();
+  int n = 10;
+
+  omp_set_dynamic(0);
+  omp_set_num_threads(4);
+
+  #pragma omp parallel shared(n)
+  {
+    #pragma omp single
+    printf ("fib(%d) = %d\n", n, fib(n));
+  }
+  finish = clock();
+    double tt = (double)(finish-start)/CLOCKS_PER_SEC;
+  	printf("Exec time is: %lf",tt);
+}
